54ab77563ec62e61a26394090e48d4d7b9642a71
issaali <issa.alali.991@gmail.com>
2024-02-05 00:01:08 +0100

    create env file

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const connection = () => {
 const connection = process.env.MONGODB_URI ;
-mongoose.connect(connection)
+const user_name = process.env.MONGODB_USER;
+const password = process.env.MONGODB_PASSWORD;
+const database = process.env.MONGODB_DATABASE;
+mongoose.connect(`mongodb+srv://${user_name}:${password}@cluster0.lew9w6o.mongodb.net/${database}?retryWrites=true&w=majority`	)
 .then(() => {
     console.log('Database connected');
 })
